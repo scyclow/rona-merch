2c91c0d22401340dede3760a90554823c5fc5d68
scyclow <spike716@gmail.com>
2020-09-29 14:16:45 -0400

    Updates

---
+++ precache-manifest.9ddad73491dbf86c9cd50767ff43e697.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d407e810802120789940cd13dbedda9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ef480311555f6222bab",
+    "url": "/static/css/main.d0b198e8.chunk.css"
+  },
+  {
+    "revision": "6359606b63bc2691e75b",
+    "url": "/static/js/2.e2adf881.chunk.js"
+  },
+  {
+    "revision": "5e900d035457dce1238b874e30f1bbc0",
+    "url": "/static/js/2.e2adf881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ef480311555f6222bab",
+    "url": "/static/js/main.226976d6.chunk.js"
+  },
+  {
+    "revision": "86b5a1fd04beee137e3a",
+    "url": "/static/js/runtime-main.98dae089.js"
+  },
+  {
+    "revision": "398581c6af964812e702c1e208ef7974",
+    "url": "/static/media/flagstar.398581c6.svg"
+  },
+  {
+    "revision": "adcbca99cbec08395150668694c479a0",
+    "url": "/static/media/flashSale.adcbca99.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8b3f098581d9be69a0a826cb95919cc3.js"
+  "/precache-manifest.9ddad73491dbf86c9cd50767ff43e697.js"
 );
 
 self.addEventListener('message', (event) => {
